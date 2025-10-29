8bf511cb8c723c9222d6f38a111afd3b20ca9e5b
indahjann <jannahindah7@gmail.com>
2025-10-30 00:04:00 +0700

    part 1b done

--- Part 1/part1/src/App.jsx
+++ Part 1/part1/src/App.jsx
@@ -1,18 +1,58 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
+const Display = ({counter}) => {
+  return (
+    <div>{counter}</div>
   )
+}
 
-  console.log('rendering....', counter)
+// const Button = (props) => {
+//   return (
+//     <button onClick={props.onClick}>
+//       {props.text}
+//     </button>
+//   )
+// }
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [ counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      {counter}
+      <Display counter={counter}/>
+
+      <Button 
+        onClick={increaseByOne}
+        text='plus'
+      />
+
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+
+      <Button 
+        onClick={decreaseByOne}
+        text='minus'
+      />
+
     </div>
   )
 }

7f77ef5fb98968f5eccd0db2e7c4abe6063c76f6
indahjann <jannahindah7@gmail.com>
2025-10-30 08:46:10 +0700

    last read: 1d-event handling revisited

--- Part 1/part1/src/App.jsx
+++ Part 1/part1/src/App.jsx
@@ -1,58 +1,17 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
-
-// const Button = (props) => {
-//   return (
-//     <button onClick={props.onClick}>
-//       {props.text}
-//     </button>
-//   )
-// }
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
-  const [ counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
+  const [value, setValue] = useState(10)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+  const handleClick = () => {
+    console.log('clicked the button')
+    setValue(0)
   }
 
   return (
     <div>
-      <Display counter={counter}/>
-
-      <Button 
-        onClick={increaseByOne}
-        text='plus'
-      />
-
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />
-
-      <Button 
-        onClick={decreaseByOne}
-        text='minus'
-      />
-
+      {value}
+      <button onClick={handleClick}>button</button>
     </div>
   )
 }

55b0fb39e70bb116ebf35f540f30d0dfa6dca708
indahjann <jannahindah7@gmail.com>
2025-10-30 15:20:48 +0700

    start part1.7

--- Part 1/unicafe/src/App.jsx
+++ Part 1/unicafe/src/App.jsx
@@ -11,10 +11,45 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+
+  const handleGoodClick = (newGoodValue) => {
+    console.log("Good Value", newGoodValue)
+    setGood(newGoodValue)
+    setTotal(newGoodValue + neutral + bad)
+    setAverage((1*newGoodValue + (-1)*bad)/3)
+  }
+
+  const handleNeutralClick = (newNeutralValue) => {
+    console.log("Neutral Value", newNeutralValue)
+    setNeutral(newNeutralValue)
+    setTotal(good + newNeutralValue + bad)
+    setAverage((1*good + (-1)*bad)/3)
+  }
+
+  const handleBadClick = (newBadValue) => {
+    console.log("Bad Value", newBadValue)
+    setBad(newBadValue)
+    setTotal(good + neutral + newBadValue)
+    setAverage((1*good + (-1)*newBadValue)/3)
+  }
+
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick={() => handleGoodClick(good + 1)} text="good"/>
+      <Button onClick={() => handleNeutralClick(neutral + 1)} text="neutral"/>
+      <Button onClick={() => handleBadClick(bad + 1)} text="bad"/>
+
+      <h1>statistic</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      
     </div>
   )
 }
